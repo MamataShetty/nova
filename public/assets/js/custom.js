a7f97e7dd7e53bd761e79337b240eecaa25ad70a
unknown <mamata.shetty990@gmail.com>
2020-07-07 22:09:50 +0530

    home page work

---
+++ public/assets/js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    //do jQuery stuff when DOM is ready
+    new TypeIt(".typed-text", {
+		  speed: 60,
+		  waitUntilVisible: true,
+		  loop: true,
+		  cursorChar: ' ',
+		})
+		  .type("Hi, I’m <br>Klara Assistant.")
+		  .pause(1000)
+		  .delete()
+		  .type("I collected <strong>109</strong><br>insurance cards today.")
+		  .pause(1000)
+			.delete()
+		  .type("I sent <strong>59</strong> pre-visit <br>instructions today.")
+		  .pause(1000)
+			.delete()
+		  .type("I rescheduled <strong>27</strong><br>no-shows today.")
+		  .pause(1000)
+			.delete()
+		  .type("I followed up with <br><strong>165</strong> patients today.")
+		  .pause(1000)
+			.delete()
+		  .type("I collected <strong>19</strong> new <br>online reviews today.")
+		  .pause(1000)
+			.delete()
+
+		  .go();
+});
