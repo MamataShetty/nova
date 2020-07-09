04abfb9cc81b9fa14ecb2d8bb2f0592f772eb33c
unknown <mamata.shetty990@gmail.com>
2020-07-09 18:55:18 +0530

    home page work in progress

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -28,11 +28,26 @@ $(document).ready(function() {
 		  .go();
 
 		$(window).scroll(function(){
-		    if ($(this).scrollTop() > 100) {
-		       $('#navbar').addClass('navbar-nova');
-		    } else {
-		       $('#navbar').removeClass('navbar-nova');
-		    }
-		});
+	    if ($(this).scrollTop() > 500) {
+	       $('#navbar').addClass('navbar-nova');
+	    } else {
+	       $('#navbar').removeClass('navbar-nova');
+	    }
+	    var scroll = $(window).scrollTop();
 
+		  if((scroll > 40)&&(scroll < 800)){
+		  	$('.telemedecine-stage-content').addClass('hide-text');
+		    $('.td-stage-woman').addClass('scale-img');
+		    $('.td-stage-notebook').addClass('scale-image-bg');
+		    $('.td-stage-phone').addClass('slide-up-bg');
+		  }
+		  else if(scroll < 40){
+
+		  	$('.telemedecine-stage-content').removeClass('hide-text');
+		    $('.td-stage-woman').removeClass('scale-img');
+		    $('.td-stage-notebook').removeClass ('scale-image-bg');
+		     $('.td-stage-phone').removeClass('slide-up-bg');
+		  }
+
+		});
 });
