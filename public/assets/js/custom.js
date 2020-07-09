3abdbd384b7c078298dc48e3e91e98741363e51e
unknown <mamata.shetty990@gmail.com>
2020-07-09 23:11:51 +0530

    animation effet added

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -33,21 +33,23 @@ $(document).ready(function() {
 	    } else {
 	       $('#navbar').removeClass('navbar-nova');
 	    }
-	    var scroll = $(window).scrollTop();
+	    if (window.innerWidth > 767) { 
+	    	var scroll = $(window).scrollTop();
 
-		  if((scroll > 40)&&(scroll < 800)){
-		  	$('.telemedecine-stage-content').addClass('hide-text');
-		    $('.td-stage-woman').addClass('scale-img');
-		    $('.td-stage-notebook').addClass('scale-image-bg');
-		    $('.td-stage-phone').addClass('slide-up-bg');
-		  }
-		  else if(scroll < 40){
-
-		  	$('.telemedecine-stage-content').removeClass('hide-text');
-		    $('.td-stage-woman').removeClass('scale-img');
-		    $('.td-stage-notebook').removeClass ('scale-image-bg');
-		     $('.td-stage-phone').removeClass('slide-up-bg');
-		  }
+			  if((scroll > 40)&&(scroll < 800)){
+			  	$(".td-stage-wrapper").removeClass("scrollhide")
+			  	$('.telemedecine-stage-content').addClass('hide-text');
+			    $('.td-stage-woman').addClass('scale-img');
+			    $('.td-stage-notebook').addClass('scale-image-bg');
+			    $('.td-stage-phone').addClass('slide-up-bg');
+			  }
+			  else if(scroll < 40){
+			  	$('.telemedecine-stage-content').removeClass('hide-text');
+			    $('.td-stage-woman').removeClass('scale-img');
+			    $('.td-stage-notebook').removeClass ('scale-image-bg');
+			     $('.td-stage-phone').removeClass('slide-up-bg');
+			  }
+			 }
 
 		});
 });
