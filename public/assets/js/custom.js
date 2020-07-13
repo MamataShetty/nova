9fc4d87c8206d9a484da261ccbf11ec8b2675531
unknown <mamata.shetty990@gmail.com>
2020-07-13 15:48:11 +0530

    provider added:\

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,31 +1,30 @@
 $(document).ready(function() {
-    //do jQuery stuff when DOM is ready
-    new TypeIt(".typed-text", {
-		  speed: 60,
-		  waitUntilVisible: true,
-		  loop: true,
-		  cursorChar: ' ',
-		})
-		  .type("Hi, I’m <br>Klara Assistant.")
-		  .pause(1000)
-		  .delete()
-		  .type("I collected <strong>109</strong><br>insurance cards today.")
-		  .pause(1000)
-			.delete()
-		  .type("I sent <strong>59</strong> pre-visit <br>instructions today.")
-		  .pause(1000)
-			.delete()
-		  .type("I rescheduled <strong>27</strong><br>no-shows today.")
-		  .pause(1000)
-			.delete()
-		  .type("I followed up with <br><strong>165</strong> patients today.")
-		  .pause(1000)
-			.delete()
-		  .type("I collected <strong>19</strong> new <br>online reviews today.")
-		  .pause(1000)
-			.delete()
+  //   new TypeIt(".typed-text", {
+		//   speed: 60,
+		//   waitUntilVisible: true,
+		//   loop: true,
+		//   cursorChar: ' ',
+		// })
+		//   .type("Hi, I’m <br>Klara Assistant.")
+		//   .pause(1000)
+		//   .delete()
+		//   .type("I collected <strong>109</strong><br>insurance cards today.")
+		//   .pause(1000)
+		// 	.delete()
+		//   .type("I sent <strong>59</strong> pre-visit <br>instructions today.")
+		//   .pause(1000)
+		// 	.delete()
+		//   .type("I rescheduled <strong>27</strong><br>no-shows today.")
+		//   .pause(1000)
+		// 	.delete()
+		//   .type("I followed up with <br><strong>165</strong> patients today.")
+		//   .pause(1000)
+		// 	.delete()
+		//   .type("I collected <strong>19</strong> new <br>online reviews today.")
+		//   .pause(1000)
+		// 	.delete()
 
-		  .go();
+		//   .go();
 
 		$(window).scroll(function(){
 	    if ($(this).scrollTop() > 200) {
